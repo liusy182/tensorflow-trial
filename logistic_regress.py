d3dad36777234f97c4a681a409b8e697670aa292
liusy182 <liusy182@gmail.com>
2017-01-29 08:12:41 +0800

    add training data

--- logistic_regress.py
+++ logistic_regress.py
@@ -15,33 +15,53 @@ def inference(X):
     return tf.sigmoid(combine_inputs(X))
 
 def loss(X, Y):
-    Y_predicted = inference(X)
-    return tf.reduce_sum(tf.squared_difference(Y, Y_predicted))
+    return tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(combine_inputs(X), Y))
 
 def inputs():
-    weight_age = [
-        [84, 46], [73, 20], [65, 52], [70, 30], [76, 57], 
-        [69, 25], [63, 28], [72, 36], [79, 57], [75, 44], 
-        [27, 24], [89, 31], [65, 52], [57, 23], [59, 60], 
-        [69, 48], [60, 34], [79, 51], [75, 50], [82, 34], 
-        [59, 46], [67, 23], [85, 37], [55, 40], [63, 30]
-    ]
-    blood_fat_content = [
-        354, 190, 405, 263, 451, 
-        302, 288, 385, 402, 365, 
-        209, 290, 346, 254, 395, 
-        434, 220, 374, 308, 220, 
-        311, 181, 274, 303, 244
-    ]
-    return tf.to_float(weight_age), tf.to_float(blood_fat_content)
+    passenger_id, survived, pclass,  name, sex, age, sibsp, parch, ticket, fare, cabin, embarked = \
+        read_csv(100, "train.csv", 
+           [[0.0], [0.0], [0], [""], [""], [0.0], [0.0], [0.0], [""], [0.0], [""], [""]]
+        )
+
+    # convert categorical data
+    is_first_class = tf.to_float(tf.equal(pclass, [1]))
+    is_second_class = tf.to_float(tf.equal(pclass, [2]))
+    is_third_class = tf.to_float(tf.equal(pclass, [3]))
+
+    gender = tf.to_float(tf.equal(sex, ["female"]))
+
+    # Finally we pack all the features in a single matrix;
+    # We then transpose to have a matrix with one example per row and one feature per column.
+    features = tf.transpose(tf.pack([is_first_class, is_second_class, is_third_class, gender, age]))
+    survived = tf.reshape(survived, [-1, 1])
+
+    return features, survived
 
 def train(total_loss):
-    learning_rate = 0.0000001
+    learning_rate = 0.01
     return tf.train.GradientDescentOptimizer(learning_rate).minimize(total_loss)
 
 def evaluate(sess, X, Y):
-    print(sess.run(inference([[80., 25.]])))
-    print(sess.run(inference([[65., 25.]])))
+    predicted   = tf.cast(inference(X) > 0.5, tf.float32)
+    print(sess.run(tf.reduce_mean(tf.cast(tf.equal(predicted, Y), tf.float32))))
+
+def read_csv(batch_size, file_name, record_defaults):
+    filename_queue = tf.train.string_input_producer(["data/" + file_name])
+
+    reader = tf.TextLineReader(skip_header_lines=1)
+    key, value = reader.read(filename_queue)
+
+    # decode_csv will convert a Tensor from type string (the text line) in
+    # a tuple of tensor columns with the specified defaults, which also
+    # sets the data type for each column
+    decoded = tf.decode_csv(value, record_defaults=record_defaults)
+
+    # batch actually reads the file and loads "batch_size" rows in a single tensor
+    return tf.train.shuffle_batch(decoded,
+                                  batch_size=batch_size,
+                                  capacity=batch_size * 50,
+                                  min_after_dequeue=batch_size,
+                                  allow_smaller_final_batch=True)
 
 
 saver = tf.train.Saver()
