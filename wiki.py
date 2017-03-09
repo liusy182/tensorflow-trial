a7a0fd818c756f19e25480221f588322fe9a105f
liusy182 <liusy182@gmail.com>
2017-03-09 08:39:19 +0800

    download not yet implemented

--- wiki.py
+++ wiki.py
@@ -3,6 +3,10 @@ import collections
 import os
 import re
 from lxml import etree
+import numpy as np
+from attrdict import AttrDict
+import tensorflow as tf
+import lazy_property
 
 TOKEN_REGEX = re.compile(r'[A-Za-z]+|[!?.:,()]')
 
@@ -79,3 +83,95 @@ class Wikipedia:
         words = cls.TOKEN_REGEX.findall(page)
         words = [x.lower() for x in words]
         return words
+
+def skipgrams(pages, max_context):
+    """Form training pairs according to the skip-gram model."""
+    for words in pages:
+        for index, current in enumerate(words):
+            context = random.randint(1, max_context)
+            for target in words[max(0, index - context): index]:
+                yield current, target
+            for target in words[index + 1: index + context + 1]:
+                yield current, target
+
+
+def batched(iterator, batch_size):
+    """Group a numerical stream into batches and yield them as Numpy arrays."""
+    while True:
+        data = np.zeros(batch_size)
+        target = np.zeros(batch_size)
+        for index in range(batch_size):
+            data[index], target[index] = next(iterator)
+        yield data, target
+
+
+
+class EmbeddingModel:
+
+    def __init__(self, data, target, params):
+        self.data = data
+        self.target = target
+        self.params = params
+        self.embeddings
+        self.cost
+        self.optimize
+
+    @lazy_property.LazyProperty
+    def embeddings(self):
+        initial = tf.random_uniform(
+            [self.params.vocabulary_size, self.params.embedding_size],
+            -1.0, 1.0)
+        return tf.Variable(initial)
+
+    @lazy_property.LazyProperty
+    def optimize(self):
+        optimizer = tf.train.MomentumOptimizer(
+            self.params.learning_rate, self.params.momentum)
+        return optimizer.minimize(self.cost)
+
+    @lazy_property.LazyProperty
+    def cost(self):
+        embedded = tf.nn.embedding_lookup(self.embeddings, self.data)
+        weight = tf.Variable(tf.truncated_normal(
+            [self.params.vocabulary_size, self.params.embedding_size],
+            stddev=1.0 / self.params.embedding_size ** 0.5))
+        bias = tf.Variable(tf.zeros([self.params.vocabulary_size]))
+        target = tf.expand_dims(self.target, 1)
+        return tf.reduce_mean(tf.nn.nce_loss(
+            weight, bias, embedded, target,
+            self.params.contrastive_examples,
+            self.params.vocabulary_size))
+
+params = AttrDict(
+    vocabulary_size=10000,
+    max_context=10,
+    embedding_size=200,
+    contrastive_examples=100,
+    learning_rate=0.5,
+    momentum=0.5,
+    batch_size=1000,
+)
+
+data = tf.placeholder(tf.int32, [None])
+target = tf.placeholder(tf.int32, [None])
+model = EmbeddingModel(data, target, params)
+
+corpus = Wikipedia(
+    'https://dumps.wikimedia.org/enwiki/20160501/' \
+    'enwiki-20160501-pages-meta-current1.xml-p000000010p000030303.bz2',
+    'tmp/wikipedia',
+    params.vocabulary_size)
+examples = skipgrams(corpus, params.max_context)
+batches = batched(examples, params.batch_size)
+
+sess = tf.Session()
+sess.run(tf.initialize_all_variables())
+average = collections.deque(maxlen=100)
+for index, batch in enumerate(batches):
+    feed_dict = {data: batch[0], target: batch[1]}
+    cost, _ = sess.run([model.cost, model.optimize], feed_dict)
+    average.append(cost)
+    print('{}: {:5.1f}'.format(index + 1, sum(average) / len(average)))
+
+embeddings = sess.run(model.embeddings)
+np.save('tmp/wikipedia/embeddings.npy', embeddings)
