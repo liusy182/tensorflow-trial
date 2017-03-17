8a081e71a5551180a579d6e4ac0de6ccfe40ebe6
liusy182 <liusy182@gmail.com>
2017-03-17 23:47:00 +0800

    feed forward propogation

--- 1_mnist_softmax.py
+++ 1_mnist_softmax.py
@@ -35,7 +35,6 @@ FLAGS = None
 def main(_):
   # Import data
   mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
-
   # Create the model
   x = tf.placeholder(tf.float32, [None, 784])
   W = tf.Variable(tf.zeros([784, 10]))
@@ -73,7 +72,7 @@ def main(_):
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
-  parser.add_argument('--data_dir', type=str, default='/tmp/tensorflow/mnist/input_data',
+  parser.add_argument('--data_dir', type=str, default='tmp/mnist/input_data',
                       help='Directory for storing input data')
   FLAGS, unparsed = parser.parse_known_args()
   tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
